7a4fea089cdcdfcea4930a2e5fa8e0b4ae2555ee
vladikhan <84037014+vladikhan@users.noreply.github.com>
2023-02-23 06:10:38 +0900

    Add files via upload

---
+++ Асинхронность (async:await).js
@@ -0,0 +1,37 @@
+const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums'
+const dataContainer = document.querySelector('.data-container')
+
+const createAlbumItem = (text) => {
+  const albumItem = document.createElement('li')
+  albumItem.innerText = text
+  return albumItem
+}
+
+const toggleLoader = () => {
+  const loaderHTML = document.querySelector('#loader')
+  const isHidden = loaderHTML.getAttribute('hidden') !== null
+  if (isHidden) {
+    loaderHTML.removeAttribute('hidden')
+  } else {
+    loaderHTML.setAttribute('hidden', '')
+  }
+}
+
+const renderAlbums = async () => {
+  toggleLoader()
+  try {
+    const response = await fetch(ALBUMS_URL)
+    const albums = await response.json()
+    albums.forEach((album) => {
+      const albumHTML = createAlbumItem(album.title)
+      dataContainer.append(albumHTML)
+    })
+  } catch {
+    dataContainer.innerText =
+      'Произошла ошибка в получении данных об альбомах...'
+  } finally {
+    toggleLoader()
+  }
+}
+
+renderAlbums()
